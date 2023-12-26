f47a16442bb0a9e7e9cb76c6f09436f979ce1d25
StoicaAndreiMarius <stoicamariuss2022@gmail.com>
2023-12-26 02:24:11 +0200

    README.mk changes.

--- README.mk
+++ README.mk
@@ -1,1 +1,8 @@
 #Sorting Algorithm Visualizer using Pygame
+
+For the source code just access "main.py", you will need to have pygame installed.
+
+You can find the executable file in the folder "dist".
+
+WARNING:
+    DO NOT CLOSE THE TERMINAL THAT OPENS WHEN RUNNING THE EXECUTABLE.
